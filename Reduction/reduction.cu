64a68dea4b188ed90a6950badae3f907e49c1d1b
jiasu <807544235@qq.com>
2023-07-14 16:49:48 +0800

    Update reduction.cu

--- Reduction/reduction.cu
+++ Reduction/reduction.cu
@@ -32,12 +32,16 @@ __global__ void GPUsum(const float* x, float* sum, const int* N)
 	}
 }
 
-// 简单的树形结构并行
+// kernel1
+// 简单的树形结构并行，存在线程分歧
+// Interleaved addressing with divergent branching
 __global__ void kernel1(float* arr, float* out)
 {
 	__shared__ float s_data[threadsPerBlock];
 	unsigned int tid = threadIdx.x;
 	unsigned int i = threadIdx.x + blockIdx.x * blockDim.x; // tid号线程负责的数组元素的位置
+	
+	// 将数据拷贝到共享内存
 	if (i < N)
 	{
 		s_data[tid] = arr[i];
@@ -46,26 +50,58 @@ __global__ void kernel1(float* arr, float* out)
 
 	for (int s = 1; s < blockDim.x; s *= 2)
 	{
-		if (tid % (2 * s) == 0 && i + s < N)
+		// 偶数线程work，例如第一论循环中，只有0、2、4、6线程执行，1、3、5线程闲置，同一个warp内有一半线程没用上
+		if (tid % (2 * s) == 0 && i + s < N) //线程分歧
 		{
 			s_data[tid] += s_data[tid + s];
 		}
 		__syncthreads();
 	}
 
+	// 拷贝规约结果，注意是Block的和，而非最终结果
 	if (tid == 0)
 	{
 		out[blockIdx.x] = s_data[0];
 	}
-
 }
 
+// kernel2
+// 解决线程分歧，存在bank conflicts
+__global__ void kernel2(float* arr, float* out)
+{
+	__shared__ float s_data[threadsPerBlock];
+	unsigned int tid = threadIdx.x;
+	unsigned int i = threadIdx.x + blockIdx.x * blockDim.x; // tid号线程负责的数组元素的位置
+
+	// 将数据拷贝到共享内存
+	if (i < N)
+	{
+		s_data[tid] = arr[i];
+	}
+	__syncthreads();
+
+	for (int s = 1; s < blockDim.x; s *= 2)
+	{
+		int index = tid * 2 * s;
+		if ((index + s) < blockDim.x && (blockIdx.x * blockDim.x + index + s) < N)
+		{
+			s_data[index] += s_data[index + s];
+		}
+		__syncthreads();
+	}
+
+	// 拷贝规约结果，注意是Block的和，而非最终结果
+	if (tid == 0)
+	{
+		out[blockIdx.x] = s_data[0];
+	}
+}
 
 
 int main(void)
 {
 	// 计时变量
-	struct timespec start, end;
+	time_ns start, end;
 
 	// 初始化数据
 	float* x = new float[N]; //源数据
@@ -93,44 +129,61 @@ int main(void)
 
 
 	/*CPU串行*/
-	timespec_get(&start, TIME_UTC);
+	start.clock();
 	// 运行CPU串行函数
 	sum = CPUsum(x);
-	timespec_get(&end, TIME_UTC);
+	end.clock();
 
 	// 输出CPU运行结果
-	printf("Calculate sum by CPU: sum = %f\nElasped time: %fms\n", sum, GetDuration(&end, &start));
+	printf("Calculate sum by CPU: sum = %f\nElasped time: %fms\n", sum, end - start);
 	printf("\n");
 
 	/*GPU单核串行*/
 	
 	// 运行GPU串行函数
-	timespec_get(&start, TIME_UTC);
-	GPUsum << <1, 1 >> > (dx, dsum, dN);
+	start.clock();
+	GPUsum <<<1, 1 >>> (dx, dsum, dN);
 
 	// 拷贝计算结果到CPU
 	cudaMemcpy(&sum, dsum, sizeof(int), cudaMemcpyDeviceToHost);
-	timespec_get(&end, TIME_UTC);
+	end.clock();
 
 	// 输出GPU串行计算结果
-	printf("Calculate sum by GPU: sum = %f\nElasped time: %fms\n", sum, GetDuration(&end, &start));
+	printf("Calculate sum by GPU: sum = %f\nElasped time: %fms\n", sum, end - start);
 	printf("\n");
 
 
 	/*GPU并行 kernel1*/
-	timespec_get(&start, TIME_UTC);
+	start.clock();
 	sum = 0;
-	kernel1 << <blocksPerGrid, threadsPerBlock >> > (dx, dy);
+	kernel1 <<<blocksPerGrid, threadsPerBlock >>> (dx, dy);
 	// 拷贝计算结果到CPU
 	cudaMemcpy(y, dy, blocksPerGrid * sizeof(float), cudaMemcpyDeviceToHost);
 	for (int i = 0; i < blocksPerGrid; i++)
 	{
 		sum += y[i];
 	}
-	timespec_get(&end, TIME_UTC);
+	end.clock();
 
 	// 输出GPU串行计算结果
-	printf("Calculate sum by GPU kernel1: sum = %f\nElasped time: %fms\n", sum, GetDuration(&end, &start));
+	printf("Calculate sum by GPU kernel1: sum = %f\nElasped time: %fms\n", sum, end - start);
+	printf("\n");
+
+
+	/*GPU并行 kernel2*/
+	start.clock();
+	sum = 0;
+	kernel2 <<<blocksPerGrid, threadsPerBlock >>> (dx, dy);
+	// 拷贝计算结果到CPU
+	cudaMemcpy(y, dy, blocksPerGrid * sizeof(float), cudaMemcpyDeviceToHost);
+	for (int i = 0; i < blocksPerGrid; i++)
+	{
+		sum += y[i];
+	}
+	end.clock();
+
+	// 输出GPU串行计算结果
+	printf("Calculate sum by GPU kernel2: sum = %f\nElasped time: %fms\n", sum, end - start);
 	printf("\n");
 
 
