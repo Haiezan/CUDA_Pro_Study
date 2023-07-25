9c92f07f6bd5d1108bd88a25b883d82d6ad63355
jiasu <807544235@qq.com>
2023-07-25 16:28:26 +0800

    test

---
+++ CudaRuntime1/kernel.cu
@@ -0,0 +1,24 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include<iostream>
+#include <time.h>
+using namespace std;
+
+__global__ void kernel()
+{
+	printf("blockIdx = %d, threadIdx = %d\n", blockIdx.x, threadIdx.x);
+	//__syncthreads();
+}
+
+int main()
+{
+	int* dn;
+	int n = 0;
+	cudaMalloc((void**)&dn, sizeof(int));
+	cudaMemcpy(dn, &n, sizeof(int), cudaMemcpyHostToDevice);
+
+	kernel <<<1, 256 >>> ();
+	cudaThreadSynchronize();
+	system("pause");
+	return 0;
+}
